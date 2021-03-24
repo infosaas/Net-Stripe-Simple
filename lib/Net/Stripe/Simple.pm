e3027dcc5a4d660901bda3eb63365a8681491519
Andy Beverley <a.beverley@ctrlo.com>
2021-03-24 06:29:14 +0000

    Remove duplicated author and copyright

--- lib/Net/Stripe/Simple.pm
+++ lib/Net/Stripe/Simple.pm
@@ -1574,25 +1574,4 @@ This software is copyright (c) 2014 by Grant Street Group.
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
-=head1 AUTHORS
-
-=over 4
-
-=item *
-
-Grant Street Group <developers@grantstreet.com>
-
-=item *
-
-David F. Houghton <dfhoughton@gmail.com>
-
-=back
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2014 by Grant Street Group.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
 =cut
